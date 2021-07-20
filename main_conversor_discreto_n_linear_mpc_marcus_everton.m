e39f6979f811183795f36ea646ea31871404a736
roscibely <rosanacibely@gmail.com>
2021-07-20 16:58:12 -0300

    Update main_conversor_discreto_n_linear_mpc_marcus_everton.m

--- main_conversor_discreto_n_linear_mpc_marcus_everton.m
+++ main_conversor_discreto_n_linear_mpc_marcus_everton.m
@@ -1,11 +1,11 @@
-%  Marcus Vinicius - Versao final 3
+%  main file
 close all, clear all, clc
 Pot=[1000 380]; Vo=48; Vg=[36 26]; L=36e-6; Co=4400e-6; Rco=26.7e-3;
 Ts=1e-3; Ta=Ts;
 g=1;h=1;
 Q1=diag([0.1 0.1 0.1],0); R1=0.1;
 umax=.5;
-% Formulação da resposta impulso para potência nominal
+% FormulaÃ§Ã£o da resposta impulso para potÃªncia nominal
 t_=0:Ts:20e-3; %gera uma curva de 20ms
 [Ahn,Bhn,An,Bn,Cn,Dn]=boost_discreto(Pot(1),Vg(1),Vo,L,Co,Rco,Ts,g,h);
 u=0;
@@ -34,7 +34,7 @@ c_=Cn+Dn*Fs;
 d_=Dn;
 Mz_Ia=a_;Mz_Ib=b_; Mz_Ic=Fs; Mz_Id=0;
 Nza=a_;Nzb=b_; Nzc=c_; Nzd=d_;
-% Condições iniciais
+% CondiÃ§Ãµes iniciais
 t=0:Ts:300e-3;
 R=[Vo*ones(1,ceil(length(t)*0.5)) Vo*ones(1,ceil(length(t)*0.5))];
 Ppot=[Pot(2) Pot(2)*ones(1,ceil((length(t)-1)*0.25)) Pot(2)*ones(1,ceil((length(t)-1)*0.25)) Pot(1)*ones(1,ceil((length(t)-1)*0.25)) Pot(1)*ones(1,ceil((length(t)-1)*0.25))];
@@ -52,7 +52,7 @@ for i=1:length(t)
     if u(i)<0, u(i)=0;end
     if u(i)>umax, u(i)=umax;end
     u_dep=u(i);
-    % Aplicação do Antiwind up
+    % AplicaÃ§Ã£o do Antiwind up
     u_til(i)=u_ant-u_dep; 
     % M(z)-I
     x1(:,i+1)=(An+Bn*Fs)*x1(:,i)+Bn*u_til(i);
@@ -61,10 +61,10 @@ for i=1:length(t)
     yd=(Cn+Dn*Fs)*x1(:,i)+Dn*u_til(i);    
     % Modelo da planta
     [A_, B_, C_]=boost_n_linear(Ppot(i),u(i),Vo,L,Co,Rco);    
-    % Saturação da corrente no indutor
+    % SaturaÃ§Ã£o da corrente no indutor
     if x(1,i)<0, x(1,i)=0;end
     if x(1,i)>105, x(1,i)=105;end     
-    % Saturação da tensão no Capacitor
+    % SaturaÃ§Ã£o da tensÃ£o no Capacitor
     if x(2,i)<0, x(2,i)=0;end
     if x(2,i)>63, x(2,i)=63;end    
     % Modelo RK4
@@ -73,7 +73,7 @@ for i=1:length(t)
     k3=A_*(x(:,i)+0.5*Ts*k2)+B_*Vvg(i);
     k4=A_*(x(:,i)+Ts*k3)+B_*Vvg(i);
     x(:,i+1)=x(:,i)+(Ts/6)*(k1+2*(k2+k3)+k4);
-    % Equação da saida
+    % EquaÃ§Ã£o da saida
     y(i)=C_*x(:,i);         
     if y(i)<0, y(i)=0;end
     y_in=y(i)+yd;    
@@ -95,10 +95,10 @@ for i=1:length(t)
     if u(i)>umax, u(i)=umax;end
     % Modelo da planta
     [A_, B_, C_]=boost_n_linear(Ppot(i),u(i),Vo,L,Co,Rco);
-    % Saturação da corrente no indutor
+    % SaturaÃ§Ã£o da corrente no indutor
     if x(1,i)<0, x(1,i)=0;end
     if x(1,i)>105, x(1,i)=105;end
-    % Saturação da tensão no Capacitor
+    % SaturaÃ§Ã£o da tensÃ£o no Capacitor
     if x(2,i)<0, x(2,i)=0;end
     if x(2,i)>63, x(2,i)=63;end
     % Modelo RK4
@@ -107,7 +107,7 @@ for i=1:length(t)
     k3=A_*(x(:,i)+0.5*Ts*k2)+B_*Vvg(i);
     k4=A_*(x(:,i)+Ts*k3)+B_*Vvg(i);
     x(:,i+1)=x(:,i)+(Ts/6)*(k1+2*(k2+k3)+k4);
-    % Equação da saida
+    % EquaÃ§Ã£o da saida
     y(i)=C_*x(:,i);      
     if y(i)<0, y(i)=0;end
     y_in=y(i)+yd;
@@ -117,7 +117,7 @@ end
 ympc=y; umpc=u; xmpc=x;
 %%
 [Fs,Qs1,Ls,gamma]=lmi_antiwindup_yalmip_n_relax(A,B,C,D);
-fprintf('Não relaxado: %.10f \n', Fs);
+fprintf('NÃ£o relaxado: %.10f \n', Fs);
 % Find Normalized Coprime factors of system [a,b,c,d].
 a_=An+Bn*Fs;
 b_=Bn;
@@ -125,7 +125,7 @@ c_=Cn+Dn*Fs;
 d_=Dn;
 Mz_Ia1=a_;Mz_Ib1=b_; Mz_Ic1=Fs; Mz_Id1=0;
 Nza1=a_;Nzb1=b_; Nzc1=c_; Nzd1=d_;
-% Condições iniciais
+% CondiÃ§Ãµes iniciais
 t=0:Ts:300e-3;
 R=[Vo*ones(1,ceil(length(t)*0.5)) Vo*ones(1,ceil(length(t)*0.5))];
 Ppot=[Pot(2) Pot(2)*ones(1,ceil((length(t)-1)*0.25)) Pot(2)*ones(1,ceil((length(t)-1)*0.25)) Pot(1)*ones(1,ceil((length(t)-1)*0.25)) Pot(1)*ones(1,ceil((length(t)-1)*0.25))];
@@ -142,7 +142,7 @@ for i=1:length(t)
     if u1(i)<0, u1(i)=0;end
     if u1(i)>umax, u1(i)=umax;end
     u_dep=u1(i);    
-    % Aplicação do Antiwind up
+    % AplicaÃ§Ã£o do Antiwind up
     u_til1(i)=u_ant-u_dep; 
     % M(z)-I
     x11(:,i+1)=(An+Bn*Fs)*x11(:,i)+Bn*u_til1(i);
@@ -151,10 +151,10 @@ for i=1:length(t)
     yd1=(Cn+Dn*Fs)*x11(:,i)+Dn*u_til1(i);   
     % Modelo da planta
     [A_, B_, C_]=boost_n_linear(Ppot(i),u1(i),Vo,L,Co,Rco);
-    % Saturação da corrente no indutor
+    % SaturaÃ§Ã£o da corrente no indutor
     if x1(1,i)<0, x1(1,i)=0;end
     if x1(1,i)>105, x1(1,i)=105;end 
-    % Saturação da tensão no Capacitor
+    % SaturaÃ§Ã£o da tensÃ£o no Capacitor
     if x1(2,i)<0, x1(2,i)=0;end
     if x1(2,i)>63, x1(2,i)=63;end  
     % Modelo RK4
@@ -163,7 +163,7 @@ for i=1:length(t)
     k3=A_*(x1(:,i)+0.5*Ts*k2)+B_*Vvg(i);
     k4=A_*(x1(:,i)+Ts*k3)+B_*Vvg(i);
     x1(:,i+1)=x1(:,i)+(Ts/6)*(k1+2*(k2+k3)+k4);
-    % Equação da saida
+    % EquaÃ§Ã£o da saida
     y1(i)=C_*x1(:,i);
     if y1(i)<0, y1(i)=0;end
     y_in=y1(i)+yd1;
